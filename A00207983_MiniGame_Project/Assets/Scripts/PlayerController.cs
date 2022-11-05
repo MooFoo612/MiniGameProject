8d15067a2b86beb84bf3b1df393dff90495ac7f4
Diarmaid de Frates <de.frates612@gmail.com>
2022-11-05 03:21:31 +0000

    Main Menu and Level One Start

---
+++ A00207983_MiniGame_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        // If input is not 0, try to move
+        if(movementInput != Vector2.zero) {
+            // Check for collisions
+            int count = rb.Cast(
+                direction,  // x and y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // Settings that determine where a collision can occur on such as layuers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // Amount to cast equal to the movement plus an offset
+            }
+        }
+    }
+
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
