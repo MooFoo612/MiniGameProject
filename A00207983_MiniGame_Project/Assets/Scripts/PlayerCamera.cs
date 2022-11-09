1a18d60df2c274f23b86ad831311563adc2596a2
MooFoo612 <83637762+MooFoo612@users.noreply.github.com>
2022-11-09 13:33:37 +0000

    Update

---
+++ A00207983_MiniGame_Project/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject camTarget;
+    public Vector3 cameraPosition = new Vector3(0,0,-1);
+
+    // Start is called before the first frame update
+    void Start()
+    {      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (camTarget) {
+            transform.position = new Vector3(
+                camTarget.transform.position.x * cameraPosition.x,
+                camTarget.transform.position.y * cameraPosition.y,
+                camTarget.transform.position.z * cameraPosition.z
+            );
+        }
+    }
+}
--- A00207983_MiniGame_Project/Assets/Scripts/PlayerController.cs
+++ A00207983_MiniGame_Project/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 4f;
+    public float collisionOffset = 0.05f;
+
+    public ContactFilter2D movementFilter;
 
     Vector2 movementInput;
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +28,23 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void FixedUpdate() {/*
+    private void FixedUpdate() {
         if(movementInput != Vector2.zero) {
             // Check for collisions
             int count = rb.Cast(
-                direction,  // x and y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementInput,  // x and y values between -1 and 1 that represent the direction from the body to look for collisions
                 movementFilter, // Settings that determine where a collision can occur on such as layuers to collide with
                 castCollisions, // List of collisions to store the found collisions into after the Cast is finished
                 moveSpeed * Time.fixedDeltaTime + collisionOffset); // Amount to cast equal to the movement plus an offset
-        }*/
+
+            if (count == 0) {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
     }
 
 
     void OnMove(InputValue movementValue) {
-       // movementInput = movementValue.Get<Vector2>();
+        movementInput = movementValue.Get<Vector2>();
     }
 }
