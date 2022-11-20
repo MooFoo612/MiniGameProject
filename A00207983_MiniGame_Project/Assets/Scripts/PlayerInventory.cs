d1da4155303292fa921617f2c0483130fde29713
Diarmaid de Frates <de.frates612@gmail.com>
2022-11-20 14:02:41 +0000

    Fixed Collectable Again

--- A00207983_MiniGame_Project/Assets/Scripts/PlayerInventory.cs
+++ A00207983_MiniGame_Project/Assets/Scripts/PlayerInventory.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    public int currency = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,28 @@ public class PlayerInventory : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Collectable") {
+
+            GameObject collectable = collision.gameObject;
+
+            int cValue = collectable.GetComponent<ItemValue>().itemValue;
+
+            currency += cValue;
+
+            Debug.Log("Collided with: " + collision.collider.name + "\nAdded " + cValue + " to Currency");
+
+            Destroy(collectable);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+
+        if (collision.collider.tag == "Collectable") {
+            
+            Debug.Log("Collision ended with: " + collision.collider.name);     
+        }  
     }
 }
